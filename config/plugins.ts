60afd4b09538df74404ab45bc4c1377b2ca4117b
Roshan Chapagain <arobce@gmail.com>
2024-08-24 20:03:56 +0545

    `add:` + plugin for contact forms

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default () => ({
+    ezforms: {
+        config: {
+            captchaProvider: {
+                name: 'none',
+            },
+            notificationProviders: [
+                {
+                    name: 'email',
+                    enabled: true,
+                    config: {
+                        subject: "RC site Contact Form",
+                        from: 'noreply@roshanchapagain.com'
+                    }
+                },
+            ]
+        }
+    }
+});
+ 
